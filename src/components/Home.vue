5d81306b502d3303a93179b1aca5d769c9887d2c
wuguixuan <237357158@qq.com>
2022-04-21 20:57:26 +0800

    t

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,18 @@
 <template>
-  <div>
-      <h3>12344</h3>
-      <h3>wgx写了内容111222</h3>
-      <h3>wgx写了内rong</h3>
-      <h3>mac第一次改了wgx的内容</h3>
-      <h3>wgx第四次写了内容</h3>
-      <h3>mac开始用这个文件夹了</h3>
+	<div>
+		<h3>12344</h3>
+		<h3>wgx写了内容111222</h3>
+		<h3>wgx写了内rong</h3>
+		<h3>mac第一次改了wgx的内容</h3>
+		<h3>wgx第四次写了内容</h3>
+		<h3>mac开始用这个文件夹了</h3>
 
-  </div>
+		<h3>dev下的001新增了内容</h3>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
